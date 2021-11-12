1a263ecf4e0e53c229a24f0f03de42954ce3db9c
Drish <drish.xd@gmail.com>
2021-11-12 22:50:03 +0530

    added todo box

--- src/components/Add Model.vue
+++ src/components/Add Model.vue
@@ -1,9 +1,44 @@
 <template>
-    <div class="relative w-full h-auto pt-20">
-        <div class="relative h-screen container-sm mx-auto px-4">
-            Box
-        </div>
+  <div class="pt-20 w-full sm:container px-4 mx-auto">
+    <div class=" w-full sm:w-3/6 mx-auto">
+
+    <div class="">
+      <h1 class="block text-center font-extrabold font-syne bg-indigo-500">Add ToDo</h1>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class="shadow sm:rounded-md sm:overflow-hidden">
+            <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+              <div class="grid grid-cols-3 gap-6">
+                <div class="col-span-3 sm:col-span-2">
+                  <label for="ToDo-Title" class="block text-sm font-medium text-gray-700">
+                    Title
+                  </label>
+                  <div class="mt-1 flex rounded-md shadow-sm">
+                    <input type="text" name="ToDo-Title" id="ToDo-Title" class="p-3 h-7 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Title" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <label for="about" class="block text-sm font-medium text-gray-700">
+                  Description
+                </label>
+                <div class="mt-1">
+                  <textarea id="about" name="about" rows="3" class="p-3 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Description" />
+                </div>
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -14,5 +49,9 @@ export default {
     .container-sm {
         max-width: 60%;
         background: #393E46;
+    }
+
+    .font-syne {
+        font-family: 'Syne';
     }
 </style>
