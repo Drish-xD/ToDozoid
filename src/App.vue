15160dd27ef8043c5ce0e4adfbec681555584614
Drish <drish.xd@gmail.com>
2021-12-12 23:32:20 +0530

    added colour

--- src/App.vue
+++ src/App.vue
@@ -32,18 +32,17 @@
             </div>
         </form>
     </div>
-    <section class="flex flex-wrap gap-6 justify-center xl:max-w-screen-2xl max-w-screen-md min-h mx-auto mt-20 px-4">
-      <div class="todos" v-for="todo in todos" :key="todo.id">
-        <div id="todo" class="max-w-xs px-6 py-3 rounded-xl cursor-default group border-2 border-gray-100 bg-purple-200 bg-opacity-50 transition-all duration-300 hover:shadow-xl hover:border-yellow-400 transform hover:translate-x-2 hover:-translate-y-2" >
+    <section class="todos flex items-start justify-center flex-wrap gap-6 xl:max-w-screen-2xl max-w-screen-md mx-auto py-10 xl:py-20 px-4">
+        <div :id="todo.id" v-for="todo in todos" :key="todo.id" class="todo max-w-xs px-6 py-3 rounded-xl cursor-default group border-2 border-gray-100 bg-purple-300 bg-opacity-50 transition-all duration-300 hover:shadow-xl hover:border-yellow-400 transform hover:translate-x-2 hover:-translate-y-2" >
             <h1 class="text-black text-xl font-extrabold break-all"> {{ todo.title }} </h1>
             <p class="text-black pt-1 text-justify break-all"> {{ todo.Description }}</p>
-            <div class="flex justify-end text-gray-800">
-                <ColorSwatchIcon class="hidden opacity-0 h-6 w-6 p-0.5 md:h-7 md:w-7 mt-1 cursor-pointer" @click="colour()"/>
+            <div class="flex justify-between text-gray-800">
+                <ColorSwatchIcon class="hidden opacity-0 icon h-6 w-6 p-0.5 md:h-7 md:w-7 mt-1 cursor-pointer transition-all duration-300 text-black hover:text-yellow-400" @click="colour(todo)"/>
                 <TrashIcon class="hidden opacity-0 icon h-6 w-6 p-0.5 md:h-7 md:w-7 mt-1 cursor-pointer transition-all duration-300 text-black hover:text-yellow-400" @click="deletetodo(todo)"/>
             </div>
         </div>
-    </div>
     </section>
+    <Footer />
   </main>
 </template>
 
@@ -51,11 +50,13 @@
 import { ref, onMounted } from "vue"
 import { TrashIcon, ColorSwatchIcon } from '@heroicons/vue/solid'
 import navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
     navbar,
+    Footer,
     TrashIcon, 
     ColorSwatchIcon,
   },
@@ -73,17 +74,21 @@ export default {
           todos.value.push(todo);
         })
       }
-      console.log(todos)
     })
    
-   function colour() {
-        console.log("colour");
+   function colour(todo) {
+      var colors = ['bg-red-300', 'bg-yellow-300', 'bg-green-300', 'bg-blue-300', 'bg-indigo-300', 'bg-purple-300' , 'bg-pink-300'];
+      var box = document.getElementById(todo.id);
+      var randomColor = colors[Math.floor(Math.random() * colors.length)];
+      box.classList.remove(colors[0], colors[1], colors[2], colors[3], colors[4], colors[5], colors[6]);
+      box.classList.add(randomColor);
+      console.log("randomColor", randomColor);
     }       
     
     function deletetodo(todo) {
         todos.value = todos.value.filter(t => t.id !== todo.id);
         localStorage.setItem("todocopy", JSON.stringify(todos.value));
-        console.log("deleted");
+        console.log("deleted", todo);
     }
 
 
@@ -99,7 +104,7 @@ export default {
           description: "",
         };
         localStorage.setItem("todocopy", JSON.stringify(todos.value));
-        console.log("add");
+        console.log("add", todos.value);
       } else {
         alert("Please enter a Title and Description");
       }
@@ -130,11 +135,7 @@ export default {
 body {
   width: 100%;
   height: 100vh;
-  background: url(./assets/pattern-dark.png);
-}
-
-.min-h {
-  min-height: 50vh;  
+  background: url(./assets/pattern-dark.png); 
 }
 
 ::-webkit-scrollbar {
@@ -154,7 +155,7 @@ body {
   background: #393E46;
 }
 
-#todo:hover .icon {
+.todo:hover .icon {
     transition: opacity 0.3s ease-in-out;
     display: block;
     opacity: 1;
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <footer class="fixed w-full bottom-0 text-sm xl:text-base text-white bg-black bg-opacity-50 text-center px-2 py-4"> &copy; Copyrights 2021 by <a class="text-yellow-400 hover:underline" href="http://drish-xd.is-a.dev/">Drish</a>. All Rights Reserved.</footer>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
